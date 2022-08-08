6b6155e6fb3c9e93108645e0097603bcc9abf4db
ivan0525 <zhuangyufeng189@gmail.com>
2022-08-08 23:44:19 +0800

    update

--- code/interface/main.go
+++ code/interface/main.go
@@ -22,7 +22,55 @@ func (ms MyString) FindVowels() []rune {
 	return vowels
 }
 
+type SalaryCalculator interface {
+	CalculatorSalary() int
+}
+
+type Permanent struct {
+	empId    int
+	basicpay int
+	pf       int
+}
+
+type Contract struct {
+	empId    int
+	basicpay int
+}
+
+func (p Permanent) CalculatorSalary() int {
+	return p.basicpay + p.pf
+}
+
+func (c Contract) CalculatorSalary() int {
+	return c.basicpay
+}
+
+func totalExpense(s []SalaryCalculator) {
+	expense := 0
+	for _, v := range s {
+		expense = expense + v.CalculatorSalary()
+	}
+	fmt.Printf("Total Expense Per Month $%d", expense)
+}
+
 func main() {
 	name := MyString("Sam Anderson")
 	fmt.Printf("name.FindVowels(): %c\n", name.FindVowels()) // name.FindVowels(): [a e o]
+	pemp1 :=
+		Permanent{
+			empId:    1,
+			basicpay: 5000,
+			pf:       20,
+		}
+	pemp2 := Permanent{
+		empId:    2,
+		basicpay: 6000,
+		pf:       30,
+	}
+	cemp1 := Contract{
+		empId:    3,
+		basicpay: 3000,
+	}
+	employees := []SalaryCalculator{pemp1, pemp2, cemp1}
+	totalExpense(employees)
 }
